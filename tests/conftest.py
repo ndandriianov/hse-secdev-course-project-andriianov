f841ee1ffbbb6ec44d611e03f1ceb7cde651bae9
nick <11andriianovnikita@gmail.com>
2025-11-25 17:36:13 +0300

    final

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ import pytest
 from httpx import AsyncClient
 from sqlmodel import SQLModel
 
-from src import app, engine
+from src.app.database import engine
+from src.app.main import app
 
 
 @pytest.fixture(scope="session", autouse=True)
